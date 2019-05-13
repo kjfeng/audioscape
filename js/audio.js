69c95857a27ce3cf15499cbd9bfd86a7124e910f
Kevin Feng <kjfeng@princeton.edu>
2019-05-13 03:22:37 -0400

    refactored building generation to generateCity.js

--- js/audio.js
+++ js/audio.js
@@ -70,109 +70,114 @@ function play() {
     // build the base geometry for each building
 
 
-    var box = new THREE.BoxGeometry( 1, 1, 1 );
-    box.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
-
-    // get rid of texture for the top of building
-    box.faceVertexUvs[0][4][0].set( 0, 0 );
-    box.faceVertexUvs[0][4][1].set( 0, 0 );
-    box.faceVertexUvs[0][4][2].set( 0, 0 );
-    box.faceVertexUvs[0][5][0].set( 0, 0 );
-    box.faceVertexUvs[0][5][1].set( 0, 0 );
-    box.faceVertexUvs[0][5][2].set( 0, 0 );
-
-    // var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    // var lambertMaterial = new THREE.MeshLambertMaterial({
-    //     color: 0xff00ee,
-    //     wireframe: true
-    // });
-    // var cube = new THREE.Mesh(geometry, lambertMaterial);
-    // cube.position.set(0, 0, 0);
-    // group.add(cube);
-
-
-    var cityGeometry= new THREE.Geometry();
-    var buildings = [];
-
-    // light and shadow colours
-    var light = new THREE.Color( 0xffffff );
-    var shadow = new THREE.Color( 0x303050 );
-
-
-    // var cityMesh = new THREE.Mesh(cityGeometry, material);
-    // group.add(cityMesh);
-
-    function generateTexture() {
-      // build a small canvas 32x64 and paint it in white
-      var canvas  = document.createElement( 'canvas' );
-      canvas.width = 32;
-      canvas.height = 64;
-      var context = canvas.getContext( '2d' );
-      // plain it in some shade of gray
-      let grayRand = Math.round(Math.random() * 255);
-      // let randomGray = rgb(redRand, blueRand, greenRand);
-      context.fillStyle = 'rgb('+ grayRand +', '+ grayRand +', '+ grayRand +')';
-      context.fillRect( 0, 0, 32, 64 );
-      // draw the window rows - with a small noise to simulate light variations in each room
-      for (var y = 2; y < 64; y += 2 ) {
-          for (var x = 0; x < 32; x += 2 ) {
-              var value   = Math.floor( Math.random() * 64 );
-              context.fillStyle = 'rgb(' + [value, value, value].join( ',' )  + ')';
-              context.fillRect( x, y, 2, 1 );
-          }
-      }
-
-      // build a bigger canvas and copy the small one in it
-      // This is a trick to upscale the texture without filtering
-      var canvas2 = document.createElement( 'canvas' );
-      canvas2.width    = 512;
-      canvas2.height   = 1024;
-      var context = canvas2.getContext( '2d' );
-      // disable smoothing
-      context.imageSmoothingEnabled        = false;
-      context.webkitImageSmoothingEnabled  = false;
-      context.mozImageSmoothingEnabled = false;
-      // then draw the image
-      context.drawImage( canvas, 0, 0, canvas2.width, canvas2.height );
-      // return the just built canvas2
-      return canvas2;
-    }
-
-    // more texture stuff
-    // generate the texture
-    var texture = new THREE.Texture( generateTexture() );
-    texture.anisotropy = renderer.getMaxAnisotropy();
-    texture.needsUpdate = true;
-
-
-
-    for (var i = 0; i < 10000; i++ ) {
-        // material.color.setRGB(Math.random(), Math.random(), Math.random());
-        // build the material
-        var material = new THREE.MeshLambertMaterial({
-          map : texture,
-          vertexColors : THREE.VertexColors
-        });
-        let randomGray = Math.random() + 0.3;
-        if (randomGray > 1) randomGray = 1;
-        material.color.setRGB(randomGray, randomGray, randomGray);
-        var buildingMesh = new THREE.Mesh(box, material);
-        // put a random position
-        buildingMesh.position.x = Math.floor( Math.random() * 200 - 100 ) * 10;
-        buildingMesh.position.z = Math.floor( Math.random() * 200 - 100 ) * 10;
-        // put a random rotation
-        buildingMesh.rotation.y = Math.random()*Math.PI*2;
-        // put a random scale
-        // MODIFY THIS FOR BUILDING SIZE?
-        buildingMesh.scale.x    = Math.random() * Math.random() * Math.random() * Math.random() * 50 + 10;
-        buildingMesh.scale.y    = (Math.random() * Math.random() * Math.random() * buildingMesh.scale.x) * 8 + 8;
-        buildingMesh.scale.z    = buildingMesh.scale.x;
-
-
-        // merge it with cityGeometry - very important for performance
-        cityGeometry.mergeMesh(buildingMesh);
-        buildings.push(buildingMesh);
-        group.add(buildingMesh);
+    // var box = new THREE.BoxGeometry( 1, 1, 1 );
+    // box.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
+    //
+    // // get rid of texture for the top of building
+    // box.faceVertexUvs[0][4][0].set( 0, 0 );
+    // box.faceVertexUvs[0][4][1].set( 0, 0 );
+    // box.faceVertexUvs[0][4][2].set( 0, 0 );
+    // box.faceVertexUvs[0][5][0].set( 0, 0 );
+    // box.faceVertexUvs[0][5][1].set( 0, 0 );
+    // box.faceVertexUvs[0][5][2].set( 0, 0 );
+    //
+    // // var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    // // var lambertMaterial = new THREE.MeshLambertMaterial({
+    // //     color: 0xff00ee,
+    // //     wireframe: true
+    // // });
+    // // var cube = new THREE.Mesh(geometry, lambertMaterial);
+    // // cube.position.set(0, 0, 0);
+    // // group.add(cube);
+    //
+    //
+    // var cityGeometry= new THREE.Geometry();
+    // var buildings = [];
+    //
+    // // light and shadow colours
+    // var light = new THREE.Color( 0xffffff );
+    // var shadow = new THREE.Color( 0x303050 );
+    //
+    //
+    // // var cityMesh = new THREE.Mesh(cityGeometry, material);
+    // // group.add(cityMesh);
+    //
+    // function generateTexture() {
+    //   // build a small canvas 32x64 and paint it in white
+    //   var canvas  = document.createElement( 'canvas' );
+    //   canvas.width = 32;
+    //   canvas.height = 64;
+    //   var context = canvas.getContext( '2d' );
+    //   // plain it in some shade of gray
+    //   let grayRand = Math.round(Math.random() * 255);
+    //   // let randomGray = rgb(redRand, blueRand, greenRand);
+    //   context.fillStyle = 'rgb('+ grayRand +', '+ grayRand +', '+ grayRand +')';
+    //   context.fillRect( 0, 0, 32, 64 );
+    //   // draw the window rows - with a small noise to simulate light variations in each room
+    //   for (var y = 2; y < 64; y += 2 ) {
+    //       for (var x = 0; x < 32; x += 2 ) {
+    //           var value   = Math.floor( Math.random() * 64 );
+    //           context.fillStyle = 'rgb(' + [value, value, value].join( ',' )  + ')';
+    //           context.fillRect( x, y, 2, 1 );
+    //       }
+    //   }
+    //
+    //   // build a bigger canvas and copy the small one in it
+    //   // This is a trick to upscale the texture without filtering
+    //   var canvas2 = document.createElement( 'canvas' );
+    //   canvas2.width    = 512;
+    //   canvas2.height   = 1024;
+    //   var context = canvas2.getContext( '2d' );
+    //   // disable smoothing
+    //   context.imageSmoothingEnabled        = false;
+    //   context.webkitImageSmoothingEnabled  = false;
+    //   context.mozImageSmoothingEnabled = false;
+    //   // then draw the image
+    //   context.drawImage( canvas, 0, 0, canvas2.width, canvas2.height );
+    //   // return the just built canvas2
+    //   return canvas2;
+    // }
+    //
+    // // more texture stuff
+    // // generate the texture
+    // var texture = new THREE.Texture( generateTexture() );
+    // texture.anisotropy = renderer.getMaxAnisotropy();
+    // texture.needsUpdate = true;
+    //
+    //
+    //
+    // for (var i = 0; i < 10000; i++ ) {
+    //     // material.color.setRGB(Math.random(), Math.random(), Math.random());
+    //     // build the material
+    //     var material = new THREE.MeshLambertMaterial({
+    //       map : texture,
+    //       vertexColors : THREE.VertexColors
+    //     });
+    //     let randomGray = Math.random() + 0.3;
+    //     if (randomGray > 1) randomGray = 1;
+    //     material.color.setRGB(randomGray, randomGray, randomGray);
+    //     var buildingMesh = new THREE.Mesh(box, material);
+    //     // put a random position
+    //     buildingMesh.position.x = Math.floor( Math.random() * 200 - 100 ) * 10;
+    //     buildingMesh.position.z = Math.floor( Math.random() * 200 - 100 ) * 10;
+    //     // put a random rotation
+    //     buildingMesh.rotation.y = Math.random()*Math.PI*2;
+    //     // put a random scale
+    //     // MODIFY THIS FOR BUILDING SIZE?
+    //     buildingMesh.scale.x    = Math.random() * Math.random() * Math.random() * Math.random() * 50 + 10;
+    //     buildingMesh.scale.y    = (Math.random() * Math.random() * Math.random() * buildingMesh.scale.x) * 8 + 8;
+    //     buildingMesh.scale.z    = buildingMesh.scale.x;
+    //
+    //
+    //     // merge it with cityGeometry - very important for performance
+    //     cityGeometry.mergeMesh(buildingMesh);
+    //     buildings.push(buildingMesh);
+    //     group.add(buildingMesh);
+    // }
+    let buildings = [];
+    let newBuildingsArr = generateCity(renderer, buildings);
+    for (let i = 0; i < newBuildingsArr.length; i++) {
+      group.add(newBuildingsArr[i]);
     }
 
     var ambientLight = new THREE.AmbientLight(0xaaaaaa);
@@ -220,8 +225,8 @@ function play() {
       // varyBuildingHeight(cube, modulate(Math.pow(lowerMaxFr, 0.8), 0, 1, 0, 8), modulate(upperAvgFr, 0, 1, 0, 4), 1);
 
       // CITY
-      for (var i = 0; i < buildings.length; i ++ ) {
-        varyBuildingHeight(buildings[i], modulate(Math.pow(lowerMaxFr, 0.8), 0, 1, 0, 8), modulate(upperAvgFr, 0, 1, 0, 4), 1);
+      for (var i = 0; i < newBuildingsArr.length; i ++ ) {
+        varyBuildingHeight(newBuildingsArr[i], modulate(Math.pow(lowerMaxFr, 0.8), 0, 1, 0, 8), modulate(upperAvgFr, 0, 1, 0, 4), 1);
     }
 
       // CONTROLS AUTO ROTATION
